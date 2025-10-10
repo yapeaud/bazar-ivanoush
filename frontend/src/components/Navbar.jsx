88b2eb3d63ab6d4aec5f3af24c9064596ed25aab
yapeaud <yapoabed@gmailcom>
2025-10-10 21:30:40 +0000

    ajout de Link au components Navbar et la mise en place de la page de connexion et d'inscription

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
                 <section className='flex items-center gap-6'>
                     <img onClick={() => (setShowSearch(true))} src={assets.search_icon} alt="" className='w-5 cursor-pointer' />
                     <article className='group relative'>
-                        <img src={assets.profile_icon} alt="" className='w-5 cursor-pointer' />
+                        <Link to='/connexion'><img src={assets.profile_icon} alt="" className='w-5 cursor-pointer' /></Link>
                         <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                             <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                                 <p className='cursor-pointer hover:text-black'>Mon profil</p>
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useState } from 'react'
 
 const LoginPage = () => {
+
+    const [curentState, setCurrentState] = useState('Inscription');
+
+    const onSubmitHandler = async (event) => {
+        event.preventDefault();
+    }
+
     return (
         <>
-
+        <section className='flex items-center justify-center' onSubmit={onSubmitHandler}>
+            <form className='flex flex-col items-center w-[90%] sm:w-96 mt-auto gap-4 text-gray-800'>
+                <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+                    <p className='prata-regular text-3xl'>{curentState}</p>
+                    <hr className='w-8 border-none h-[1.5px] bg-gray-800' />
+                </div>
+                {curentState === 'Connexion' ? '' : <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Nom' required />}
+                <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Adresse e-mail' required />
+                <input type="password" className='w-full px-3 py-2 border border-gray-800' placeholder='Mot de passe' required />
+                <div className='w-full flex justify-between text-sm mt-[-8px]'>                   
+                    <p className='cursor-pointer'>Mot de passe oublié?</p>
+                    {
+                        curentState === 'Connexion' 
+                        ? <p className='cursor-pointer' onClick={() => setCurrentState('Inscription')}>Créer un compte</p>
+                        :<p className='cursor-pointer' onClick={() => setCurrentState('Connexion')}> Connectez-vous ici</p>
+                    }
+                </div>
+                <button type="submit" className='bg-black text-white font-light px-8 py-2 mt-4'>{curentState === 'Connexion' ? 'Se connecter' : 'S\'inscrire'}</button>
+            </form>
+        </section>
+            
         </>
     )
 }
