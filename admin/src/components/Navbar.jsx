eba689837b11554250d31e0c64c4150e0764bafc
yapeaud <yapoabed@gmailcom>
2025-10-28 23:03:36 +0000

    Modification de vite.config.js et l'mplémentation du dossier admin

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <section>
+                <img src="" alt="" />
+                <button></button>
+            </section>
+        </>
+    )
+}
+
+export default Navbar
---
+++ admin/src/pages/OrdersPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OrdersPage = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default OrdersPage
